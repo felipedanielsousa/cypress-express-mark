263f45e2cddfbef6cc4adbc97f5d191f7c12e37e
felipedanielsousa <felipedanielsousa@gmail.com>
2023-03-09 00:26:36 -0300

    criação de função customizada

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -3,24 +3,61 @@
 describe('tarefas', ()=>{
 
     it('Deve cadastrar uma nova tarefa', ()=>{
-
+        
+        const taskName = 'Ler um livro de node.js'
+        
         cy.request({
             url: 'http://localhost:3333/helper/tasks',
             method: 'DELETE',
-            body: {name: 'Ler um livro de node.js'}
+            body: {name: 'taskName'}
         }).then(response => {
             expect(response.status).to.eq(204)
         })
 
-        cy.visit('http://localhost:8080')
-
-        cy.get('input[placeholder="Add a new Task"]')
-            .type('Ler um livro de node.js')
-
-        ////button[contains(text(), "Create")]
-        cy.contains('button', 'Create').click()
+        cy.createTask(taskName)
         
-        cy.contains('main div p','Ler um livro de node.js')
+        cy.contains('main div p',taskName)
             .should('be.visible')
     }) 
+
+    it('Não deve permitir tarefa duplicada', () =>{
+        
+        const task = {
+            name: 'Estudar Javascript',
+            is_done: false
+        }
+        
+        cy.request({
+            url: 'http://localhost:3333/helper/tasks',
+            method: 'DELETE',
+            body: {name: task.name}
+        }).then(response => {
+            expect(response.status).to.eq(204)
+        })
+        
+        cy.request({
+             url: 'http://localhost:3333/tasks',
+             method: 'POST',
+             body: task
+        }).then(response => {
+            expect(response.status).to.eq(201)
+        })
+
+        cy.createTask(task.name)
+
+        cy.get('.swal2-html-container')
+            .should('be.visible')
+            .should('have.text', 'Task already exists!')
+    })
+
+})
+
+Cypress.Commands.add('createTask', (taskName) => {
+        
+    cy.visit('http://localhost:8080')
+
+    cy.get('input[placeholder="Add a new Task"]')
+        .type(taskName)
+    ////button[contains(text(), "Create")]
+    cy.contains('button', 'Create').click()
 })
