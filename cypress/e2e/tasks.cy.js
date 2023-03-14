e81b475384538237f8154a8041132684d9efa076
felipedanielsousa <felipedanielsousa@gmail.com>
2023-03-14 09:07:19 -0300

    allure server report

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -2,92 +2,95 @@
 
 describe('tarefas', ()=>{
 
-    it('Deve cadastrar uma nova tarefa', ()=>{
-<<<<<<< HEAD
+    let testData;
 
-        const taskName = 'Ler um livro de node.js'
-
-        cy.request({
-            url: 'http://localhost:3333/helper/tasks',
-            method: 'DELETE',
-            body: {name: taskName}
-        }).then(response => {
-            expect(response.status).to.eq(204)
+    before(()=>{
+        cy.fixture('tasks').then(t => {
+            testData = t
         })
-
-        cy.visit('http://localhost:8080')
-
-        cy.get('input[placeholder="Add a new Task"]')
-            .type(taskName)
-
-        ////button[contains(text(), "Create")]
-        cy.contains('button', 'Create').click()
-        
-=======
-        
-        const taskName = 'Ler um livro de node.js'
-        
-        cy.removeTaskByName(taskName)
-        cy.createTask(taskName)
-        
->>>>>>> 0f9e1d9a863aaa6c9702b629ab07c08e7eb5350d
-        cy.contains('main div p',taskName)
-            .should('be.visible')
-    }) 
-
-<<<<<<< HEAD
-    it('Não permitir devoluções duplicadas', () =>{
-=======
-    it('Não deve permitir tarefa duplicada', () =>{
->>>>>>> 0f9e1d9a863aaa6c9702b629ab07c08e7eb5350d
-        
-        const task = {
-            name: 'Estudar Javascript',
-            is_done: false
-        }
-        
-<<<<<<< HEAD
-        cy.request({
-            url:'http://localhost:3333/helper/tasks',
-            method:'DELETE',
-            body: {name: task.name}
-        }).then(response => {
-            expect(response.status).to.eq(204)
-        })
-
-        cy.request({
-            url:'http://localhost:3333/tasks',
-            method:'POST',
-            body: task
-        }).then(response => {
-            expect(response.status).to.eq(201)
-        })
-
-        cy.visit('http://localhost:8080')
-
-        cy.get('input[placeholder="Add a new Task"]')
-            .type(task.name)
-
-        ////button[contains(text(), "Create")]
-        cy.contains('button', 'Create').click()
-=======
-        cy.removeTaskByName(task.name)
-        cy.postTask(task)
-        cy.createTask(task.name)
->>>>>>> 0f9e1d9a863aaa6c9702b629ab07c08e7eb5350d
-
-        cy.get('.swal2-html-container')
-            .should('be.visible')
-            .should('have.text', 'Task already exists!')
     })
-<<<<<<< HEAD
-})
-=======
 
-    it('Campo obrigatório', () => {
-        cy.createTask()
-        cy.isRequired('This is a required field')
+
+    context('cadastro', () => {
+        it('Deve cadastrar uma nova tarefa', ()=>{
+
+            const taskName = 'Ler um livro de node.js'
+    
+            cy.removeTaskByName(taskName)
+            cy.createTask(taskName)
+            
+            cy.contains('main div p',taskName)
+                .should('be.visible')
+        }) 
+    
+        it('Não deve permitir tarefa duplicada', () =>{
+            
+            const task = testData.dup
+            
+            cy.removeTaskByName(task.name)
+            cy.postTask(task)
+            cy.createTask(task.name)
+    
+            cy.get('.swal2-html-container')
+                .should('be.visible')
+                .should('have.text', 'Task already exists!')
+        })
+    
+        it('Campo obrigatório', () => {
+            cy.createTask()
+            cy.isRequired('This is a required field')
+        })
     })
+
+    context('atualizações', ()=> {
+        it('deve concluir uma tarefa', () => {
+            
+            const task = testData.dup
+
+            
+
+            cy.removeTaskByName(task.name)
+            cy.postTask(task)
+
+            cy.visit('/')
+            //(//p[contains(text(), "Pagar contas de consumo")]/..//button)[1]
+            cy.contains('p', task.name)
+                .parent()
+                //* funciona como um contains, pega uma parte do class que nao vai mudar e combina com p
+                .find('button[class*=ItemToggle]') 
+                .click()
+
+            cy.contains('p', task.name)
+                .should('have.css','text-decoration-line','line-through')
+
+        })
+    })
+
+    context('exclusão', ()=> {
+        it('deve remover uma tarefa', () => {
+            
+            const task = {
+                name: 'Estudar Javascript',
+                is_done: false
+            }
+
+            cy.removeTaskByName(task.name)
+            cy.postTask(task)
+
+            cy.visit('/')
+            //(//p[contains(text(), "Pagar contas de consumo")]/..//button)[1]
+            cy.contains('p', task.name)
+                .parent()
+                //* funciona como um contains, pega uma parte do class que nao vai mudar e combina com p
+                .find('button[class*=ItemDeleteButton]') 
+                .click()
+
+            cy.contains('p', task.name)
+                .should('not.exist')
+
+        })
+    })
+
+
 })
 
->>>>>>> 0f9e1d9a863aaa6c9702b629ab07c08e7eb5350d
